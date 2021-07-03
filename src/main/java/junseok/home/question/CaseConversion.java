0b7e7c6efbb80b11e5ddce9a2ccc86e448f3c858
Junseok Oh <ixtears@naver.com>
2021-07-04 02:23:37 +0900

    feat(string) : 대소문자 변환

---
+++ src/main/java/junseok/home/question/CaseConversion.java
@@ -0,0 +1,24 @@
+package junseok.home.question;
+
+public class CaseConversion {
+
+    // A ~ Z : 65 ~ 90
+    // a ~ z : 97 - 122
+    // input : StuDY
+    // output : sTUdy
+    public String solution(String input) {
+
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (c >= 65 && c <= 90) {
+                answer.append(Character.toLowerCase(c));
+            }
+            if (c >= 97 && c <= 122) {
+                answer.append(Character.toUpperCase(c));
+            }
+        }
+
+        return answer.toString();
+    }
+}
