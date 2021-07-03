c74dc2f5a388c7b0eeddd6f547376ba8f622165c
연구개발팀장 <ojs@ibdata.co.kr>
2021-07-04 01:29:03 +0900

    test(string) : 문자열 안에서 문자 찾기 테스트
    1. 파라미터 하나만 받는 테스트
    2. 복수의 파라미터를 받아서 연속 실행되는 테스트

--- src/main/test/junseok/home/question/FindACharacterTest.java
+++ src/main/test/junseok/home/question/FindACharacterTest.java
@@ -1,5 +1,76 @@
 package junseok.home.question;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FindACharacterTest {
 
+    private FindACharacter findACharacter;
+
+    @BeforeEach
+    void setUp() {
+        findACharacter = new FindACharacter();
+    }
+
+    @Test
+    @DisplayName("문자열과 문자가 주어졌을 때 문자열에 포함된 문자의 개수 구하기")
+    void solutionTest() {
+
+        // given
+        String str = "Computercoolerc";
+        char ch = 'c';
+
+        // when
+        int count = findACharacter.solution(str, ch);
+
+        // then
+        assertThat(count).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringAndChar")
+    void solutionTests(String str, char c, int answer) {
+
+        // when
+        int count = findACharacter.solution(str, c);
+
+        // then
+        assertThat(count).isEqualTo(answer);
+    }
+
+    static Stream<Arguments> stringAndChar() {
+        return Stream.of(
+                arguments(
+                        "Computercoolerc", 'c', 3
+                ),
+                arguments(
+                        "Computercooler", 'c', 2
+                )
+        );
+    }
+
+
+
+    @Test
+    void noting() {
+
+        String str = "cococ";
+        char ch = 'o';
+        int i = str.indexOf(ch);
+        String substring = str.substring(i + 1);
+
+        String str2 = "";
+        int o = str2.indexOf("o");
+        System.out.println(o);
+    }
 
 }
