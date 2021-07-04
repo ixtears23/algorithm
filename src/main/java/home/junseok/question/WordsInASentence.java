a0b2f12eb5a0072eae8c108b20184592ed8458a9
Junseok Oh <ixtears@naver.com>
2021-07-04 23:55:58 +0900

    feat(string) : 문장 속 단어(최대값 구하는 방식)

--- src/main/java/home/junseok/question/WordsInASentence.java
+++ src/main/java/home/junseok/question/WordsInASentence.java
@@ -16,4 +16,35 @@ public class WordsInASentence {
         return answer;
     }
 
+    public String solution2(String input) {
+
+        String[] strArray = input.split(" ");
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+
+        for (String str : strArray) {
+            int len = str.length();
+            if (len > m) {
+                m = len;
+                answer = str;
+            }
+        }
+
+        return answer;
+    }
+
+    public String solution3(String input) {
+
+        String[] strArray = input.split(" ");
+        String answer = "";
+
+        for (String str : strArray) {
+            if (str.length() > answer.length()) {
+                answer = str;
+            }
+        }
+
+        return answer;
+    }
+
 }
