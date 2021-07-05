e0bc6e99e6c8ab4c3475084f04f684dc668c9312
연구개발팀장 <ojs@ibdata.co.kr>
2021-07-05 15:26:49 +0900

    feat(string) : 회문 문자열

---
+++ src/main/java/home/junseok/question/PalindromeString.java
@@ -0,0 +1,39 @@
+package home.junseok.question;
+
+public class PalindromeString {
+    public String solution(String input) {
+        String answer = "YES";
+
+        char[] chars = input.toUpperCase().toCharArray();
+
+        int lt = 0;
+        int rt = input.length() - 1;
+
+        while (lt < rt) {
+
+            if (chars[lt] != chars[rt]) {
+                answer = "NO";
+                break;
+            }
+            lt++;
+            rt--;
+        }
+        return  answer;
+    }
+
+    public String solution2(String input) {
+        String answer = "YES";
+
+        input = input.toUpperCase();
+
+        int len = input.length();
+
+        for (int i = 0; i < len/2; i++) {
+            if (input.charAt(i) != input.charAt(len-i-1)) {
+                return "NO";
+            }
+        }
+
+        return answer;
+    }
+}
