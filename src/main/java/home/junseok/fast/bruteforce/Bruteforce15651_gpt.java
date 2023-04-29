ea543d0276cbe70ef10255415cc3890d0ef1ee5f
ixtears23 <ixtears23@icloud.com>
2023-04-29 12:34:21 +0900

    백준[15651] gpt 풀이 추가

---
+++ src/main/java/home/junseok/fast/bruteforce/Bruteforce15651_gpt.java
@@ -0,0 +1,44 @@
+package home.junseok.fast.bruteforce;
+
+import java.util.Scanner;
+
+/**
+ * 완전탐색[1]
+ * https://www.acmicpc.net/problem/15651
+ * 문제 : 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ * - 1부터 N까지 자연수 중에서 M개를 고른 수열
+ * - 같은 수를 여러 번 골라도 된다.
+ *
+ *
+ *
+ *
+ */
+public class Bruteforce15651_gpt {
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        arr = new int[m];
+
+        dfs(n, m, 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            arr[depth] = i;
+            dfs(n, m, depth + 1);
+        }
+    }
+}
