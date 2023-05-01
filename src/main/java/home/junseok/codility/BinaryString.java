4e9e5a1bb0a02af2851ae562a2a11ef7fc812c91
ixtears23 <ixtears23@icloud.com>
2023-05-02 08:32:54 +0900

    코딜리티(기본) - 이진 갭 길이 구하기 성공

---
+++ src/main/java/home/junseok/codility/BinaryString.java
@@ -0,0 +1,50 @@
+package home.junseok.codility;
+
+public class BinaryString {
+
+    private static final StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        toBinaryString(1162);
+        System.out.println(getBinaryGap());
+    }
+
+    private static int getBinaryGap() {
+        final String binayString = sb.reverse().toString();
+        System.out.println(binayString);
+
+        int max = 0;
+        int temp = 0;
+
+        boolean isStart = false;
+        boolean isEnd = false;
+
+        for (char b : binayString.toCharArray()) {
+            if (b == '1' && !isStart) {
+                isStart = true;
+            } else if (isStart && b == '0') {
+                temp++;
+            } else if (b == '1' && isStart) {
+                isEnd = true;
+            }
+
+            if (isStart && isEnd) {
+                isEnd = false;
+                if (max < temp) {
+                    max = temp;
+                }
+                temp = 0;
+            }
+        }
+
+        return max;
+    }
+
+    private static void toBinaryString(int n) {
+        while (n > 0) {
+            int bit = n % 2;
+            sb.append(bit);
+            n /= 2;
+        }
+    }
+
+}
